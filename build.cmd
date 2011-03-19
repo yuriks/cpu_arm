09bf6c66a7788c010ea7cf57840c1bdf77ad2aa8
yuriks <yuriks.br@gmail.com>
2011-03-19 18:25:41 -0300

    Rudimentary cmd.exe build system.

---
+++ build.cmd
@@ -0,0 +1,16 @@
+@echo off
+set BUILD_FILES=general.vhdl buffer.vhdl decoder16.vhdl latch.vhdl mux16.vhdl mux2.vhdl reg.vhdl register_file.vhdl
+if [%1] neq [] (
+	set BUILD_FILES=%BUILD_FILES% %1%.vhdl
+)
+for %%i in (%BUILD_FILES%) do (
+	echo Analyzing %%i
+	ghdl -a %%i
+)
+if [%1] neq [] (
+	echo Elaborating...
+	ghdl -e %1
+	echo Running...
+	ghdl -r %1 --vcd=waves.vcd
+)
+echo on
